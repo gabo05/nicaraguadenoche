f8598a15016f696e9180ff61e69c15c80e9faa20
gabo05 <san.gaby05@gmail.com>
2018-02-08 00:03:09 -0600

    Disable sessions

--- config/session.js
+++ config/session.js
@@ -96,12 +96,12 @@ module.exports.session = {
   //     ssl: true
   //   }
   // }
-  adapter: 'connect-mongo',
+  //adapter: 'connect-mongo',
   // Note: user, pass and port are optional
-  url: 'mongodb://nicaraguadenoche:ZRkdmwcygEFjMkrT@ds135486.mlab.com:35486/nicaraguadenoche',
+  //url: 'mongodb://nicaraguadenoche:ZRkdmwcygEFjMkrT@ds135486.mlab.com:35486/nicaraguadenoche',
   // url: 'mongodb://localhost:27017/nicaraguadenoche',
-  collection: 'sessions',
-  auto_reconnect: true,
-  ssl: false,
-  stringify: true
+  // collection: 'sessions',
+  // auto_reconnect: true,
+  // ssl: false,
+  // stringify: true
 };
